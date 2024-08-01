475673dcf09277304ec0cbfe2bbfa12935400562
yoon3nam <lgmysn2@gmail.com>
2024-08-01 15:39:38 +0900

    chore: our에서 tailwind/globals.css 옮겨옴

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,48 @@
 import type { Config } from "tailwindcss";
 
+const plugin = require("tailwindcss/plugin");
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        mo: { max: "1023px" },
+        pc: "1024px",
+      },
+      colors: {
+        "border-gray": "#dadce0",
+      },
+      zIndex: {
+        layout: "100",
+      },
+      spacing: {
+        xxxs: "2px",
+        xxs: "4px",
+        xs: "8px",
+        sm: "12px",
+        md: "16px",
+        lg: "20px",
+        xl: "24px",
+        xxl: "32px",
+        xxxl: "40px",
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }: { addUtilities: any }) {
+      addUtilities({
+        ".inner": {},
+        ".x-center": {},
+        ".y-center": {},
+        ".center": {},
+      });
+    }),
+  ],
 };
 export default config;
