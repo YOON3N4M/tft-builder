974a78104a1c33434d7396668cbb104b60e629c1
yoon3nam <lgmysn2@gmail.com>
2024-08-01 14:45:01 +0900

    test: ItemCobination 컴포넌트 적용 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
+import ItemCombination from "@/components/ItemCombination";
+import { CORE_ITEM_LIST } from "@/constants/item";
+import { ITEM_ICON_URL } from "@/constants/url";
 import Image from "next/image";
 import { useEffect } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <ItemCombination />
+    </main>
   );
 }
