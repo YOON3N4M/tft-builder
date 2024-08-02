5ec4f9d3eecf668cc8c36d57cc758bf1da815acd
yoon3nam <lgmysn2@gmail.com>
2024-08-02 13:39:13 +0900

    feat: Overlay 템플릿 컴포넌트
    드래그 기능이 있는 컴포넌트들은 해당 컴포넌트를 활용해
    구성하면 된다

---
+++ src/components/overlay/Overlay.tsx
@@ -0,0 +1,49 @@
+import { cn } from "@/utils";
+import { HTMLAttributes, ReactNode, useState } from "react";
+import { useDrag } from "react-use-gesture";
+
+export interface OverlayProps extends HTMLAttributes<HTMLDivElement> {
+  children?: ReactNode;
+  x?: number;
+  y?: number;
+  /**
+   * 오버레이를 닫아도 상태가 유지 되어야 하는 경우 해당
+   *
+   * prop로 hidden
+   */
+  hidden?: boolean;
+}
+
+/**
+ * 드래그가 가능한 overlay 컨텐츠들에
+ *
+ * 활용 가능한 템플릿 컴포넌트
+ */
+function Overlay(props: OverlayProps) {
+  const { children, className, x = 0, y = 0, hidden } = props;
+
+  const [pos, setPos] = useState({ x: x, y: y });
+
+  const bindPos = useDrag((params) => {
+    setPos({
+      x: params.offset[0],
+      y: params.offset[1],
+    });
+  });
+
+  return (
+    <div
+      className={cn(
+        className,
+        "relative cursor-pointer z-overlay border shadow-sm rounded-[4px] bg-white",
+        hidden && "hidden"
+      )}
+      {...bindPos()}
+      style={{ top: pos.y, left: pos.x }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Overlay;
