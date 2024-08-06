7ea7875510d02b58318a6eba8b6e985fde516b36
yoon3nam <lgmysn2@gmail.com>
2024-08-06 16:10:05 +0900

    fix: build error 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 
-import ItemCombination from "@/components/ItemCombination";
-import { CORE_ITEM_LIST } from "@/constants/item";
-import { ITEM_ICON_URL } from "@/constants/url";
 import BuilderContainer from "@/containers/BuilderContainer";
-import Image from "next/image";
-import { useEffect } from "react";
 
 export default function Home() {
   return (
