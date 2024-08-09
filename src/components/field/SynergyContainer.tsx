bc315c691bbfbca751a764d39bb91daeb762f686
yoon3nam <lgmysn2@gmail.com>
2024-08-09 11:14:01 +0900

    refactor: 컴포넌트 분리

---
+++ src/components/field/SynergyContainer.tsx
@@ -0,0 +1,188 @@
+import { checkGrade, cn, groupBy, sortByNumber } from "@/utils";
+import { IndexedChampion } from "./Field";
+import { Tooltip, useTooltip } from "../tooltip/Tooltip";
+import { Champion, SET_12_CHAMPIONS } from "@/constants/champions";
+import Image from "next/image";
+import { CHAMPION_ICON_URL } from "@/constants/url";
+import { Arrow } from "../svgs";
+import { Synergy } from "@/constants/synergy";
+
+interface SynergyContainerProps {
+  indexedChampionList: IndexedChampion[];
+}
+
+const synergyBgStyles: { [key: string]: string } = {
+  unranked: "bg-gray-900",
+  bronze: "bg-[#a0715e]",
+  silver: "bg-[#7c8f92]",
+  gold: "bg-[#bd9a38]",
+  prism: "bg-[#ad1457]",
+};
+
+function SynergyContainer(props: SynergyContainerProps) {
+  const { indexedChampionList } = props;
+
+  //console.log(indexedChampionList);
+
+  const duplicateRemoves = removeDuplicateSyenrgy(indexedChampionList);
+
+  const synergyList = duplicateRemoves.flatMap(
+    (indexedChampion) => indexedChampion.champion.synergy
+  );
+
+  const refinedSynergyList = groupBy(synergyList, "name");
+  const sortByLength = refinedSynergyList.sort((a, b) => b.length - a.length);
+  const soltByGrade = refinedSynergyList.sort((a, b) => {
+    const grade: any = {
+      unranked: 1,
+      bronze: 2,
+      silver: 3,
+      gold: 4,
+      prism: 5,
+    };
+
+    return grade[checkGrade(b).gradeText] - grade[checkGrade(a).gradeText];
+  });
+
+  // 동일한 챔피언이 배치되어 있을때
+  function removeDuplicateSyenrgy(indexedChampionList: IndexedChampion[]) {
+    const seen = new Set(); // 중복을 추적할 Set 생성
+    return indexedChampionList.filter((champion) => {
+      const keyValue = champion.champion["name"]; // 현재 요소의 key 값
+      if (seen.has(keyValue)) {
+        return false; // 이미 본 key 값이면 false 반환
+      }
+      seen.add(keyValue); // 새로운 key 값이면 Set에 추가
+      return true; // true 반환하여 결과 배열에 포함
+    });
+  }
+
+  return (
+    <div className="flex flex-col gap-sm py-md max-h-[400px] w-[90%]">
+      {sortByLength.length === 0 && (
+        <div className="w-[95%] p-sm bg-white border rounded-md opacity-70">
+          챔피언 배치시 시너지가 활성화 됩니다{" "}
+        </div>
+      )}
+      {sortByLength.map((synergy) => (
+        <SynergyListItem
+          indexedChampionList={indexedChampionList}
+          key={synergy[0].name}
+          synergy={synergy}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default SynergyContainer;
+
+const borderColorStyles: { [key: string]: string } = {
+  "1": "border-tier-1",
+  "2": "border-tier-2",
+  "3": "border-tier-3",
+  "4": "border-tier-4",
+  "5": "border-tier-5",
+};
+
+interface SyenrgyListItemProps {
+  indexedChampionList: IndexedChampion[];
+  synergy: Synergy[];
+}
+
+function SynergyListItem(props: SyenrgyListItemProps) {
+  const { indexedChampionList, synergy } = props;
+  const { isTooltipOn, tooltipOn, tooltipOff } = useTooltip();
+
+  const synergyItem = synergy[0];
+  const synergyChampions = SET_12_CHAMPIONS.filter((champion) =>
+    champion.synergy.some((synergy) => synergy.name === synergyItem.name)
+  );
+
+  const sortByTier = sortByNumber(synergyChampions, "tier");
+
+  function championExist(champion: Champion) {
+    const exist = indexedChampionList.find(
+      (cham) => cham.champion.name === champion.name
+    );
+
+    return exist ? true : false;
+  }
+
+  return (
+    <div
+      key={synergyItem.name}
+      onMouseOver={tooltipOn}
+      onMouseLeave={tooltipOff}
+      className="relative flex items-center border p-xs text-sm w-[95%] bg-white rounded-md"
+    >
+      <Tooltip className="" isTooltipOn={isTooltipOn}>
+        <div className="font-semibold">{synergyItem.name}</div>
+        <div className="mt-sm flex gap-xxs">
+          {sortByTier.map((champion) => (
+            <div
+              key={champion.name}
+              className={cn(
+                "size-[40px] overflow-hidden flex rounded-md border-2",
+                borderColorStyles[champion.tier.toString()],
+                !championExist(champion) && "!opacity-50"
+              )}
+            >
+              <Image
+                src={CHAMPION_ICON_URL(champion.src)}
+                width={256}
+                height={128}
+                alt={champion.name}
+                className="object-cover relative object-[-35px_0px] scale-125"
+              />
+            </div>
+          ))}
+        </div>
+      </Tooltip>
+      <div
+        className={cn(
+          "p-xxs hexagon w-[34px] h-[36px] flex items-center justify-center",
+          synergyBgStyles[checkGrade(synergy)?.gradeText]
+        )}
+      >
+        <Image
+          width={22}
+          height={22}
+          src={`/images/synergy/${synergyItem.src[0]}.png`}
+          alt={synergyItem.name}
+          className="filter"
+        />
+      </div>
+      <div
+        className={cn(
+          "px-xs ml-[-5px] text-white",
+          synergyBgStyles[checkGrade(synergy)?.gradeText]
+        )}
+      >
+        {synergy.length}
+      </div>
+      <div className="ml-xs flex flex-col">
+        <div className="flex">
+          <span>{synergyItem.name}</span>
+        </div>
+        <div className="flex items-center gap-xxxs">
+          {synergyItem.requirQty.map((qty, idx) => (
+            <>
+              <span
+                className={cn(
+                  "text-gray-500 text-xs",
+                  checkGrade(synergy)?.gradeNumber === qty && "!text-black"
+                )}
+              >
+                {qty}
+              </span>
+              {idx + 1 !== synergyItem.requirQty.length && (
+                <Arrow size={10} className="inline fill-gray-300" />
+              )}
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
