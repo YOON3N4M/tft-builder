ade4b4834be908b19f0bf0ff9f7e44865b4ef25c
yoon3nam <lgmysn2@gmail.com>
2024-08-27 16:09:21 +0900

    fix(refactor): BuildCopyButton, add tooltip

--- src/components/BuildCopyButton.tsx
+++ src/components/BuildCopyButton.tsx
@@ -2,18 +2,32 @@
 
 import { copyBuild } from "@/utils/localstorage";
 import { Clipboard } from "./svgs";
+import { ToolTip, useToolTip } from "./tooltips/ToolTip";
 
 interface BuildCopyButtonProps {
   url: string;
+  isTooltip?: boolean;
 }
 
 function BuildCopyButton(props: BuildCopyButtonProps) {
-  const { url } = props;
+  const { url, isTooltip = false } = props;
+
+  const { isTooltipOn, tooltipOff, tooltipOn, tooltipContainerRef, pos } =
+    useToolTip();
 
   return (
-    <button onClick={() => copyBuild(url)}>
-      <Clipboard />
-    </button>
+    <div className="relative" ref={tooltipContainerRef}>
+      <button
+        onMouseEnter={tooltipOn}
+        onMouseLeave={tooltipOff}
+        onClick={() => copyBuild(url)}
+      >
+        <Clipboard />
+      </button>
+      <ToolTip position="bottom" x={pos.x} y={pos.y} isOn={isTooltipOn}>
+        <p>챔피언의 배치 위치는 복사되지 않으므로 재배치가 필요합니다.</p>
+      </ToolTip>
+    </div>
   );
 }
 
