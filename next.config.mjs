5f73f7661008917d6720c1f242f65b20eb7c3e43
윤세남 <lgmysn2@gmail.com>
2024-09-12 10:38:46 +0900

    chore: riot tft api key

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    GA_ID: process.env.GA_ID,
-    ENV: process.env.ENV,
-    ADSENSE_ID: process.env.ADSENSE_ID,
-  },
-  async redirects() {
-    return [{ source: "/", destination: "/builder", permanent: false }];
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ddragon.leagueoflegends.com",
-      },
-    ],
-  },
-};
+	env: {
+		RIOT_TFT_API_KEY: process.env.RIOT_TFT_API_KEY,
+		GA_ID: process.env.GA_ID,
+		ENV: process.env.ENV,
+		ADSENSE_ID: process.env.ADSENSE_ID,
+	},
+	async redirects() {
+		return [{ source: '/', destination: '/builder', permanent: false }]
+	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'ddragon.leagueoflegends.com',
+			},
+		],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
--- src/services/riot.ts
+++ src/services/riot.ts
@@ -1,124 +1,122 @@
 import {
-  AccountDto,
-  RiotAccountRes,
-  RiotId,
-  RiotMatchInfoRes,
-  RiotSummonerRes,
-  RiotTftLeagueRes,
-  RiotTftMatchListRes,
-  SummonerData,
-} from "@/types/riot";
+	AccountDto,
+	RiotAccountRes,
+	RiotId,
+	RiotMatchInfoRes,
+	RiotSummonerRes,
+	RiotTftLeagueRes,
+	RiotTftMatchListRes,
+	SummonerData,
+} from '@/types/riot'
 
-import { filterNull } from "@/utils";
+import { filterNull } from '@/utils'
 
-const TEMP_API_KEY = "RGAPI-1d453e58-2717-4e5c-9965-6bc2c72a42fc";
-
-const RIOT_API_KEY = TEMP_API_KEY;
+const RIOT_API_KEY = process.env.RIOT_TFT_API_KEY
 
 export async function refreshRiotData(riotId: RiotId) {
-  const riotData: SummonerData = {
-    account: null,
-    summoner: null,
-    league: null,
-    matchInfoList: null,
-  };
+	const riotData: SummonerData = {
+		account: null,
+		summoner: null,
+		league: null,
+		matchInfoList: null,
+	}
 
-  const accountRes = await getRiotAccount(riotId);
+	const accountRes = await getRiotAccount(riotId)
 
-  if (!accountRes) return riotData;
+	if (!accountRes) return riotData
 
-  riotData.account = accountRes;
+	riotData.account = accountRes
 
-  const summonerRes = await getTftSummoner(accountRes.puuid);
-  if (!summonerRes) return riotData;
+	const summonerRes = await getTftSummoner(accountRes.puuid)
+	if (!summonerRes) return riotData
 
-  riotData.summoner = summonerRes;
+	riotData.summoner = summonerRes
 
-  const leagueRes = await getTftLeague(summonerRes.id);
-  if (!leagueRes) return riotData;
+	const leagueRes = await getTftLeague(summonerRes.id)
+	if (!leagueRes) return riotData
 
-  riotData.league = leagueRes;
+	riotData.league = leagueRes
 
-  const matchIdListRes = await getTftMatchList(accountRes.puuid);
+	const matchIdListRes = await getTftMatchList(accountRes.puuid)
 
-  if (!matchIdListRes) return riotData;
+	if (!matchIdListRes) return riotData
 
-  const matchInfoList = await Promise.all(
-    matchIdListRes.map(async (id) => await getTftMatchInfo(id))
-  );
+	const matchInfoList = await Promise.all(
+		matchIdListRes.map(async (id) => await getTftMatchInfo(id)),
+	)
 
-  const filtered = filterNull(matchInfoList) as RiotMatchInfoRes[];
+	const filtered = filterNull(matchInfoList) as RiotMatchInfoRes[]
 
-  riotData.matchInfoList = filtered;
+	riotData.matchInfoList = filtered
 
-  return riotData;
+	return riotData
 }
 
 export async function getRiotAccount(
-  riotId: RiotId
+	riotId: RiotId,
 ): Promise<RiotAccountRes | null> {
-  const url = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${riotId.gameName}/${riotId.tagLine}?api_key=${RIOT_API_KEY}`;
+	const url = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${riotId.gameName}/${riotId.tagLine}?api_key=${RIOT_API_KEY}`
 
-  const data = await riotApiFetch(url);
+	const data = await riotApiFetch(url)
 
-  return data;
+	return data
 }
 export async function getRiotAccountByPuuid(
-  puuid: string
+	puuid: string,
 ): Promise<AccountDto | null> {
-  const url = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-puuid/${puuid}?api_key=${RIOT_API_KEY}`;
+	const url = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-puuid/${puuid}?api_key=${RIOT_API_KEY}`
 
-  const data = await riotApiFetch(url);
-  return data;
+	const data = await riotApiFetch(url)
+	return data
 }
 export async function getTftSummoner(
-  puuid: string
+	puuid: string,
 ): Promise<RiotSummonerRes | null> {
-  const url = `https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/${puuid}?api_key=${RIOT_API_KEY}`;
+	const url = `https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-puuid/${puuid}?api_key=${RIOT_API_KEY}`
 
-  const data = await riotApiFetch(url);
-  return data;
+	const data = await riotApiFetch(url)
+	return data
 }
 
 export async function getTftLeague(
-  summonerId: string
+	summonerId: string,
 ): Promise<RiotTftLeagueRes[] | null> {
-  const url = `https://kr.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${RIOT_API_KEY}`;
+	const url = `https://kr.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerId}?api_key=${RIOT_API_KEY}`
 
-  const data = await riotApiFetch(url);
-  return data;
+	const data = await riotApiFetch(url)
+	return data
 }
 
 export async function getTftMatchList(
-  puuid: string
+	puuid: string,
 ): Promise<RiotTftMatchListRes | null> {
-  const url = `https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/${puuid}/ids?start=0&count=10&api_key=${RIOT_API_KEY}`;
+	const url = `https://asia.api.riotgames.com/tft/match/v1/matches/by-puuid/${puuid}/ids?start=0&count=10&api_key=${RIOT_API_KEY}`
 
-  const data = await riotApiFetch(url);
+	const data = await riotApiFetch(url)
 
-  return data;
+	return data
 }
 
 export async function getTftMatchInfo(
-  matchId: string
+	matchId: string,
 ): Promise<RiotMatchInfoRes | null> {
-  const url = `https://asia.api.riotgames.com/tft/match/v1/matches/${matchId}?api_key=${RIOT_API_KEY}`;
+	const url = `https://asia.api.riotgames.com/tft/match/v1/matches/${matchId}?api_key=${RIOT_API_KEY}`
 
-  const data = await riotApiFetch(url);
-  return data;
+	const data = await riotApiFetch(url)
+	return data
 }
 
 export async function riotApiFetch(url: string, option?: RequestInit) {
-  try {
-    const res = await fetch(url, option);
-    const result = await res.json();
+	try {
+		const res = await fetch(url, option)
+		const result = await res.json()
 
-    if (result.status && result.status.status_code !== 200) {
-      return null;
-    }
+		if (result.status && result.status.status_code !== 200) {
+			return null
+		}
 
-    return result;
-  } catch (error) {
-    return null;
-  }
+		return result
+	} catch (error) {
+		return null
+	}
 }
